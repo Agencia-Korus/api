99a29b5269c09d7b382edbae2258476cc5b59a9b
lcsgborges <lcsgborges@gmail.com>
2026-04-06 16:21:50 -0300

    feat: add docker compose

--- api/app.py
+++ api/app.py
@@ -6,6 +6,6 @@ from fastapi.responses import JSONResponse
 app = FastAPI(title='API Korus')
 
 
-@app.get('/', response_class=JSONResponse, status_code=HTTPStatus.OK)
+@app.get('/health', response_class=JSONResponse, status_code=HTTPStatus.OK)
 async def health():
 	return {'message': 'API is running...'}
