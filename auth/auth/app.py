83b591313c2b279f0df427ce631b4973d898137b
lcsgborges <lcsgborges@gmail.com>
2026-05-21 15:31:03 -0300

    chore: reestrutura pastas do projeto

---
+++ auth/auth/app.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title='Korus Auth Service',
+    version='0.1.0',
+    description='Microsserviço de autenticação'
+)
--- core/enums.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-from typing import Iterable
-
-def enum_values(cls: type[Enum]) -> Iterable[str]:
-    return [member.value for member in cls]
