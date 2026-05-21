bcfa52ffb600c4c61e85e5540a9c6c530ab31391
lcsgborges <lcsgborges@gmail.com>
2026-05-21 19:08:23 -0300

    feat: adiciona microserviço de autenticação

--- auth/auth/app.py
+++ auth/auth/app.py
@@ -5,3 +5,7 @@ app = FastAPI(
     version='0.1.0',
     description='Microsserviço de autenticação'
 )
+
+@app.get('/health', tags=['Health Auth'])
+async def health():
+    return {'status': 'auth microservice is running'}
