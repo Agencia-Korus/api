09d1be40ad483a176165e28d712044f368991793
BrzGab <gabriellopes16001@gmail.com>
2026-05-22 16:31:26 -0300

    fix: adiciona as rotas de projetos, tarefas e dashboard no router

--- api/api/v1/router.py
+++ api/api/v1/router.py
@@ -1,6 +1,17 @@
 from fastapi import APIRouter
 from modules.users.controller import router as users_router
+from modules.projetos.controller import router as projetos_router
+from modules.tarefas.controller import router as tarefas_router
+from modules.dashboard.controller import router as dashboard_router
+
+
 
 api_router = APIRouter()
 
 api_router.include_router(users_router)
+api_router.include_router(projetos_router)
+api_router.include_router(tarefas_router)
+api_router.include_router(dashboard_router)
+
+
+
