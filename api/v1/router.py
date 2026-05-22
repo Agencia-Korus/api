2ea26b97a942063458f8ffc07a778088bd463be8
Artur Mendonça Arruda <arturmendoncaarruda@gmail.com>
2026-05-21 23:37:23 -0300

    feat: implementa modulo de portfolio de cases e destaques

--- api/v1/router.py
+++ api/v1/router.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
+from modules.portfolio.controller import router as portfolio_router
 from modules.posts.controller import router as posts_router
 from modules.servicos.controller import router as servicos_router
 
 api_router = APIRouter()
 
+api_router.include_router(portfolio_router)
 api_router.include_router(posts_router)
 api_router.include_router(servicos_router)
